62be2ba64341ba8810bfc7ba074c9014235940bb
RogerRochoRibas <98771907+RogerRochoRibas@users.noreply.github.com>
2022-07-26 15:28:04 +0200

    improved gallery

--- src/components/Chat.js/gallery.js
+++ src/components/Chat.js/gallery.js
@@ -3,12 +3,27 @@ import Carousel from "better-react-carousel";
 
 const Gallery = () => {
   return (
-    <Carousel cols={2} rows={1} gap={10} loop>
+    <Carousel cols={6} rows={1} gap={10} loop>
       <Carousel.Item>
         <img width="100%" src="https://picsum.photos/800/600?random=1" />
       </Carousel.Item>
       <Carousel.Item>
         <img width="100%" src="https://picsum.photos/800/600?random=2" />
+      </Carousel.Item>
+      <Carousel.Item>
+        <img width="100%" src="https://picsum.photos/800/600?random=3" />
+      </Carousel.Item>
+      <Carousel.Item>
+        <img width="100%" src="https://picsum.photos/800/600?random=4" />
+      </Carousel.Item>
+      <Carousel.Item>
+        <img width="100%" src="https://picsum.photos/800/600?random=5" />
+      </Carousel.Item>
+      <Carousel.Item>
+        <img width="100%" src="https://picsum.photos/800/600?random=6" />
+      </Carousel.Item>
+      <Carousel.Item>
+        <img width="100%" src="https://picsum.photos/800/600?random=7" />
       </Carousel.Item>
       <Carousel.Item>
         <img width="100%" src="https://picsum.photos/800/600?random=3" />
