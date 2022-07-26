18af3479aa1b947e12820adec41b025745fdd610
Hiena <aledaldin@gmail.com>
2022-07-26 13:08:32 +0200

    actualizacion de cards users

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,24 +3,28 @@ import CardGroup from "react-bootstrap/CardGroup";
 import { Link } from "react-router-dom";
 import Row from "react-bootstrap/Row";
 import { useState, useEffect } from "react";
+import Badge from "react-bootstrap/Badge";
 
 function Home() {
   // States
   const [users, setusers] = useState([
     {
-      user_name: "Carles Artola",
+      name: "Carles Artola",
       recomendaciones: 230,
       img: "https://contents.mediadecathlon.com/s927758/k$5c1763bb50956dff71179a7e19806815/4%20itemsliderdestacado%20500x500%20juegos%20de%20playa.jpg?format=auto",
+      experto: [{ playa: "snorkel" }, { montaña: "senderismo" }],
     },
     {
-      user_name: "Daniel Lopez",
+      name: "Daniela Lopez",
       recomendaciones: 530,
-      img: "https://contents.mediadecathlon.com/s927758/k$5c1763bb50956dff71179a7e19806815/4%20itemsliderdestacado%20500x500%20juegos%20de%20playa.jpg?format=auto",
+      img: "https://contents.mediadecathlon.com/s938933/k$9df2f3bb699d81bd65343a6a70f28b71/b%20hp%20verano%20mal%20432x432.jpg?format=auto",
+      experto: [{ playa: "snorkel" }, { montaña: "senderismo" }],
     },
     {
-      user_name: "Joan Bosch",
+      name: "Joan Bosch",
       recomendaciones: 230,
-      img: "https://contents.mediadecathlon.com/s927758/k$5c1763bb50956dff71179a7e19806815/4%20itemsliderdestacado%20500x500%20juegos%20de%20playa.jpg?format=auto",
+      img: "https://contents.mediadecathlon.com/s938936/k$fb5a970485b93f15f692fd0521c7dc47/b%20hp%20verano%20acc%20432x432.jpg?format=auto",
+      experto: [{ playa: "snorkel" }, { montaña: "senderismo" }],
     },
   ]);
   const [loading, setLoading] = useState(false);
@@ -40,17 +44,20 @@ function Home() {
         {!loading ? (
           users.map((u) => (
             <Card className="shadow">
-              <Card.Img
-                variant="top"
-                src="https://contents.mediadecathlon.com/s927758/k$5c1763bb50956dff71179a7e19806815/4%20itemsliderdestacado%20500x500%20juegos%20de%20playa.jpg?format=auto"
-              />
+              <Card.Img variant="top" src={u.img} />
               <Card.Body>
-                <Card.Title>user</Card.Title>
-                <Card.Text>
-                  This is a wider card with supporting text below as a natural
-                  lead-in to additional content. This content is a little bit
-                  longer.
+                <Card.Title>{u.name}</Card.Title>
+                <Card.Text className="text-muted">
+                  Recomendaciones: {u.recomendaciones}
                 </Card.Text>
+                <div className="d-flex gap-2">
+                  <Badge pill bg="primary">
+                    {u.experto.map((c) => c.montaña)}
+                  </Badge>
+                  <Badge pill bg="primary">
+                    {u.experto.map((c) => c.playa)}
+                  </Badge>
+                </div>
               </Card.Body>
               <Card.Footer>
                 <small className="text-muted">
